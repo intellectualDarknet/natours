3273840edf59e9a160225f122b9eba9988eaa492
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-02-01 20:21:11 +0500

    feat: 144 set security headers, add helmet, limited weight of possible requests

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const rateLimit = require('express-rate-limit')
-
+const helmet = require('helmet')
 const express = require('express');
 const morgan = require('morgan');
-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -21,14 +20,20 @@ const limiter = rateLimit({
   // allow 100 request per 1h
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this API, please try again in an hour',
-  standardHeaders: true
 });
+
+//set security HTTP headers 
+app.use(helmet())
 
 //use before every request GG
 // in this case use for api
 app.use('/api/', limiter);
 
-app.use(express.json());
+// Body parse, reading data from body into req.body
+// limits our requets we can sent only 10kb the same thing
+// according to the our app request
+app.use(express.json({ limit: '10kb'}));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
