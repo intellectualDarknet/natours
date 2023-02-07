46dbb930585a116d97f6dbed5524d20527ac4d21
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-02-07 18:22:28 +0500

    feat: 99 page, limit for pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,6 +50,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v')
     }
 
+    // 5) Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+    query = query.skip(skip).limit(limit)
+
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('this page does not exist')
+    }
+
     const tours = await query
 
     res.status(200).json({
@@ -62,7 +74,6 @@ exports.getAllTours = async (req, res) => {
       message: e
     })
   }
-
 
 };
 
