1408e89a19c88e0d949e38cd3eb6919467fc39f8
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-02-21 19:34:27 +0500

    feat: 206 email service in dev sends messages to mailtrap and in prod to user general implementation

--- utils/email.js
+++ utils/email.js
@@ -1,37 +1,60 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug')
+const htmlToText = require('html-to-text')
 
-const sendEmail = async options => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    // user -who we want to send and url -where
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    // configure email address in env
+    this.from = `Jonas some fellow <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    //differs in prod and dev
+    if (process.env.NODE_ENV === 'production') {
+      //  just skip for now
+      return 1 
     }
-  });
 
-  const mailOptions = {
-    from: 'Jonas some fellow <trash@smash.io>',
-    to: options.email,
-    subject: options.subject,
-    email: options.email,
-    text: options.message
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+  }
 
-  await transporter.sendMail(mailOptions);
-};
+  async send(template, subject) {
+    // 1) Render HTML bbased on a pug template
+    // we dont want to actually render but to create
+    // html so we can send this html as emai
+    // we can pass data to the pug file
+    const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url
+    })
 
-// smth like server
-// 1) create a transporter
+    // 2) Define email options
 
-// service: 'Gmail',
-// auth: {
-//   user: process.env.EMAIL_USERNAME,
-//   password: process.env.EMAIL_PASSWORD
-// }
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      // we need a way of converting html into simple text so
+      text: htmlToText.fromString(html)
+    };
 
-// Activate in gmail "less secure app" option
-// Gmail only allows 500 messages per day and surely u ll be banned as spammer
-// lets fake mails Mailtrap
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions)
+  }
 
-module.exports = sendEmail;
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to the Natours Family!')
+  }
+}
