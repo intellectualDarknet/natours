40a05626bca90e5f8fa5635103bd31df88c1720e
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-02-21 13:41:41 +0500

    feat: add default img and update user when downloading img

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -17,9 +17,9 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
 });
 
 exports.updateOne = Model => catchAsync(async (req, res, next) => {
-  console.log('updateOne')
-  console.log('request file', req.file)
-  console.log('request body', req.body)
+
+  if (req.file) req.body.photo = req.file.filename
+
     const doc = await Model.findByIdAndUpdate(req.params.id || req.user.id, req.body, {
       new: true,
       runValidators: true
@@ -29,13 +29,14 @@ exports.updateOne = Model => catchAsync(async (req, res, next) => {
       return next(new AppError('No tour found with that ID', 404));
     }
 
+
     res.status(200).json({
       status: 'success',
       data: {
         doc
       }
     });
-  });
+});
 
 exports.createOne = Model => catchAsync(async (req, res, next) => {
   const newTour = await Model.create(req.body);
