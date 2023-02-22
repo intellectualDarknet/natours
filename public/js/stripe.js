a6bb4ad6d698cdbf6075f8aee8c0489ef05fccc7
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-02-22 21:02:25 +0500

    feat: 212, 213

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -4,13 +4,13 @@ export const bookTour = async tourId => {
   // 1) Get checkout session from API
   console.log('book tour')
   const session = await axios(`http://localhost:5000/api/v1/booking/checkout-session/${tourId}`)
-
+  console.log('session', session)
     // 2) Create checkout form + charge credit card
-    // await stripe.redirectToCheckout({
-    //   sessionId: session.data.session.id
-    // });
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
 
-    if (session) window.location.href = session.data.session.url;
+    // if (session) window.location.href = session.data.session.url;
 
   console.log(session)
 }
